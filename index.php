4a7d491135af3535a5f9f393fe8359c464fc0d62
Elton Schivei Costa <costa@elton.schivei.nom.br>
2014-09-29 18:22:13 -0300

    Initial libraries

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include_once './silly/libraries/cli/Colors.php';
 
+use \silly\libraries\cli\Colors as Color;
+
+echo Color::getColoredString("Teste", Color::FG_CYAN) . "\n" . PHP_OS . "\n";
